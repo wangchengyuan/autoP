32e765ab91c3486aa2ce3d7b9af223ae03b79657
zhangfenghua <zhangfenghua@iyunxiao.com>
2017-03-23 17:37:48 +0800

    数据更新

--- data/yjdata.py
+++ data/yjdata.py
@@ -1,5 +1,5 @@
 loginname='15810798241'
 loginpasswd='798241'
 subjectid='1004957'
-testurl="ct.yunxiao.com:8110"
+testurl="http://ct.yunxiao.com:8110"
 subjecturl="http://ct.yunxiao.com:8110/subject/1004888"
