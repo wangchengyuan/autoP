a6a47ba6f2dd3504d305e72b4a119a83f06af2db
zhangfenghua <zhangfenghua@iyunxiao.com>
2017-03-27 16:35:28 +0800

    更新配置文件

--- data/yjdata.py
+++ data/yjdata.py
@@ -3,5 +3,6 @@ loginpasswd='798241'
 subjectid='1004957'
 testurl="ct.yunxiao.com:8110"
 subjecturl="http://ct.yunxiao.com:8110/subject/1004888"
+subjecturl2="http://ct.yunxiao.com:8110/subject/1005205"
 studentdatafile="D:\\autoP\\data\\teststudent.xls"
 
