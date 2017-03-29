282d1dc1680ef811f0394dde99267a6b541eb6c5
wangchengyuan <784444045@qq.com>
2017-03-29 10:25:45 +0800

    修改case为免登陆

--- case/yj/testexam.py
+++ case/yj/testexam.py
@@ -10,93 +10,97 @@ from data import yjdata
 
 
 class Exam(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        global driver
+        logi = loginorout.Loginorout
+        driver=webdriver.Chrome()
+        #driver = webdriver.PhantomJS(executable_path=r'C:\python\phantomjs\bin\phantomjs.exe')
+        driver.get("http://ct.yunxiao.com:8110")
+        logi.login(cls,driver, yjdata.loginname, yjdata.loginpasswd)
+        time.sleep(3)
 
-    def setUp(self):
-        self.logi = loginorout.Loginorout
-        self.driver=webdriver.Chrome()
-        #self.driver = webdriver.PhantomJS(executable_path=r'C:\python\phantomjs\bin\phantomjs.exe')
-        self.driver.get("http://ct.yunxiao.com:8110")
-
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        driver.quit()
 
     def test1_createexam(self):
         #创建考试
         global examname
         examname="automationtest"+time.strftime('%Y%m%d%H%M%S')
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.CSS_SELECTOR,"body > header > nav > span > a").click()
-        self.driver.find_element(By.CSS_SELECTOR,"body > div.main-content--center > div:nth-child(2) > div:nth-child(3) > input").send_keys(examname)
-        self.driver.find_element(By.ID,"examedit_starttime").send_keys(Keys.ENTER)
-        self.driver.find_element(By.ID,"examedit_endtime").send_keys(Keys.ENTER)
+        driver.find_element(By.CSS_SELECTOR,"body > header > nav > span > a").click()
+        driver.find_element(By.CSS_SELECTOR,"body > div.main-content--center > div:nth-child(2) > div:nth-child(3) > input").send_keys(examname)
+        driver.find_element(By.ID,"examedit_starttime").send_keys(Keys.ENTER)
+        driver.find_element(By.ID,"examedit_endtime").send_keys(Keys.ENTER)
         time.sleep(1)
-        self.driver.find_element(By.ID,"template_11").click()
-        self.driver.find_element(By.ID,"examedit_save").click()
+        driver.find_element(By.ID,"template_11").click()
+        driver.find_element(By.ID,"examedit_save").click()
         time.sleep(4)
-        self.assertTrue(self.driver.find_element(By.LINK_TEXT,examname))
+        self.assertTrue(driver.find_element(By.LINK_TEXT,examname))
 
 
     def test2_checkexamdynamic(self):
        #检查创建考试动态
-        self.logi.login(self, self.driver, yjdata.loginname, yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.LINK_TEXT,examname).click()
+        driver.find_element(By.LINK_TEXT,examname).click()
         time.sleep(1)
-        self.driver.find_element(By.CLASS_NAME,"star-empty").click()
+        driver.find_element(By.CLASS_NAME,"star-empty").click()
         time.sleep(4)
-        content=self.driver.find_element(By.CSS_SELECTOR,"body > div.feed.main-content--center > div.feed__rightContent > div.feed__rightContent__exam-list > ul > li > a > p.item").text
+        content=driver.find_element(By.CSS_SELECTOR,"body > div.feed.main-content--center > div.feed__rightContent > div.feed__rightContent__exam-list > ul > li > a > p.item").text
         self.assertIn(examname,content,"动态中不存在创建考试的动态")
 
     def test3_checkexam_usermanage(self):
         #检查学生管理页面是否显示正常
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.LINK_TEXT,examname).click()
+        driver.find_element(By.LINK_TEXT,examname).click()
         time.sleep(1)
-        self.driver.find_element(By.ID,"exam-detail-people-kaosheng-second-active").click()
+        driver.find_element(By.ID,"exam-detail-people-kaosheng-second-active").click()
         time.sleep(1)
-        content=self.driver.find_element(By.CSS_SELECTOR,"#studentInfo > thead > tr > th:nth-child(2)").text
+        content=driver.find_element(By.CSS_SELECTOR,"#studentInfo > thead > tr > th:nth-child(2)").text
         self.assertEqual('学号',content,"学号不存在，页面有问题")
 
     def test4_checkexam_studentdata(self):
         #导入数据
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.LINK_TEXT,examname).click()
+        driver.find_element(By.LINK_TEXT,examname).click()
         time.sleep(1)
-        self.driver.find_element(By.ID,"exam-detail-people-kaosheng-second-active").click()
+        driver.find_element(By.ID,"exam-detail-people-kaosheng-second-active").click()
         time.sleep(2)
-        self.driver.find_element(By.NAME,"file").send_keys(yjdata.studentdatafile)
+        driver.find_element(By.NAME,"file").send_keys(yjdata.studentdatafile)
         time.sleep(8)
-        self.driver.find_element(By.CLASS_NAME,"btn-close").click()
+        driver.find_element(By.CLASS_NAME,"btn-close").click()
         time.sleep(2)
-        content=self.driver.find_element(By.ID,"exam-kaosheng-school-overview-left").text
+        content=driver.find_element(By.ID,"exam-kaosheng-school-overview-left").text
         self.assertIn('63',content,"上传有误")
 
     def test5_checkexam_editexam(self):
         #检查修改考试页面
-         self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+         driver.get("http://ct.yunxiao.com:8110")
          time.sleep(3)
-         self.driver.find_element(By.LINK_TEXT,examname).click()
+         driver.find_element(By.LINK_TEXT,examname).click()
          time.sleep(1)
-         self.driver.find_element(By.CLASS_NAME,"edit-2").click()
+         driver.find_element(By.CLASS_NAME,"edit-2").click()
          time.sleep(2)
-         self.assertTrue(self.driver.find_element(By.ID,"examedit_save"),"页面有问题")
+         self.assertTrue(driver.find_element(By.ID,"examedit_save"),"页面有问题")
 
     def test6_checkexam_deleteexam(self):
         #删除考试
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.LINK_TEXT,examname).click()
+        driver.find_element(By.LINK_TEXT,examname).click()
         time.sleep(1)
-        self.driver.find_element(By.CLASS_NAME,"delete").click()
+        driver.find_element(By.CLASS_NAME,"delete").click()
         time.sleep(1)
-        self.driver.find_element(By.CSS_SELECTOR,"#yx_messager_danger_3 > div.yx-dialog-wrapper.yx-dialog-danger > div.yx-dialog-footer > a.yx-dialog-btn.yx-dialog-btn-danger").click()
+        driver.find_element(By.CSS_SELECTOR,"#yx_messager_danger_3 > div.yx-dialog-wrapper.yx-dialog-danger > div.yx-dialog-footer > a.yx-dialog-btn.yx-dialog-btn-danger").click()
         time.sleep(1)
         tag=True
         try:
-            self.driver.find_element(By.LINK_TEXT,examname)
+            driver.find_element(By.LINK_TEXT,examname)
         except NoSuchElementException:
             tag=False
         self.assertFalse(tag,"删除失败")
--- case/yj/testusermanager.py
+++ case/yj/testusermanager.py
@@ -10,38 +10,43 @@ from data import studentinfo
 
 
 class  UserManage(unittest.TestCase):
-    def setUp(self):
-        self.logi = loginorout.Loginorout
-        self.driver=webdriver.Chrome()
-        #self.driver = webdriver.PhantomJS(executable_path=r'C:\python\phantomjs\bin\phantomjs.exe')
-        self.driver.get("http://ct.yunxiao.com:8110")
+    @classmethod
+    def setUpClass(cls):
+        global driver
+        logi = loginorout.Loginorout
+        driver = webdriver.Chrome()
+        # driver = webdriver.PhantomJS(executable_path=r'C:\python\phantomjs\bin\phantomjs.exe')
+        driver.get("http://ct.yunxiao.com:8110")
+        logi.login(cls, driver, yjdata.loginname, yjdata.loginpasswd)
+        time.sleep(3)
 
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        driver.quit()
 
     def test1_checkstudentmanage(self):
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
+        driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
         time.sleep(2)
-        self.assertTrue(self.driver.find_element(By.ID,"studentTitle"))
+        self.assertTrue(driver.find_element(By.ID,"studentTitle"))
 
     def test2_addstudentinfo(self):
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.CLASS_NAME, "header__schoolBase").click()
+        driver.find_element(By.CLASS_NAME, "header__schoolBase").click()
         time.sleep(2)
-        self.driver.find_element(By.ID,"addSingle").click()
+        driver.find_element(By.ID,"addSingle").click()
         time.sleep(2)
-        self.driver.find_element(By.CSS_SELECTOR,"#add-single > div > div > div:nth-child(3) > input").send_keys(studentinfo.Sname)
-        self.driver.find_element(By.CSS_SELECTOR,"#add-single > div > div > div:nth-child(4) > input").send_keys(studentinfo.Sid)
-        self.driver.find_element(By.CSS_SELECTOR, "#add-single > div > div > div:nth-child(6) > input").send_keys(studentinfo.SEntryyear)
-        grade=self.driver.find_element(By.NAME, "nianji")
+        driver.find_element(By.CSS_SELECTOR,"#add-single > div > div > div:nth-child(3) > input").send_keys(studentinfo.Sname)
+        driver.find_element(By.CSS_SELECTOR,"#add-single > div > div > div:nth-child(4) > input").send_keys(studentinfo.Sid)
+        driver.find_element(By.CSS_SELECTOR, "#add-single > div > div > div:nth-child(6) > input").send_keys(studentinfo.SEntryyear)
+        grade=driver.find_element(By.NAME, "nianji")
         Select(grade).select_by_value(studentinfo.Sgrade)
-        self.driver.find_element(By.CSS_SELECTOR, "#add-single > div > div > div:nth-child(9) > input").send_keys(studentinfo.Sgrade)
-        self.driver.find_element(By.ID,"addBtn").click()
+        driver.find_element(By.CSS_SELECTOR, "#add-single > div > div > div:nth-child(9) > input").send_keys(studentinfo.Sgrade)
+        driver.find_element(By.ID,"addBtn").click()
         time.sleep(10)
-        elements=self.driver.find_elements(By.CLASS_NAME,"stu-name")
+        elements=driver.find_elements(By.CLASS_NAME,"stu-name")
         tag=False
         for element in elements:
             if element.text=='天涯':
@@ -49,14 +54,14 @@ class  UserManage(unittest.TestCase):
         self.assertTrue(tag,"人员信息添加不成功")
 
     def test3_searchstudent(self):
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
+        driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
         time.sleep(2)
-        self.driver.find_element(By.ID,"searchInfo").send_keys(studentinfo.Sname)
-        self.driver.find_element(By.ID,"searchInfo").send_keys(Keys.ENTER)
+        driver.find_element(By.ID,"searchInfo").send_keys(studentinfo.Sname)
+        driver.find_element(By.ID,"searchInfo").send_keys(Keys.ENTER)
         time.sleep(2)
-        elements=self.driver.find_elements(By.CLASS_NAME,"stu-name")
+        elements=driver.find_elements(By.CLASS_NAME,"stu-name")
         tag=False
         for element in elements:
             if element.text=='天涯':
@@ -64,44 +69,44 @@ class  UserManage(unittest.TestCase):
         self.assertTrue(tag,"查询刚创建学生不成功")
 
     def test4_deletestudent(self):
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
-        time.sleep(6)
-        self.driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
+        driver.get("http://ct.yunxiao.com:8110")
+        time.sleep(3)
+        driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
         time.sleep(2)
-        self.driver.find_element(By.ID,"searchInfo").send_keys(studentinfo.Sname)
-        self.driver.find_element(By.ID,"searchInfo").send_keys(Keys.ENTER)
+        driver.find_element(By.ID,"searchInfo").send_keys(studentinfo.Sname)
+        driver.find_element(By.ID,"searchInfo").send_keys(Keys.ENTER)
         time.sleep(2)
-        elements=self.driver.find_elements(By.CLASS_NAME,"stu-name")
+        elements=driver.find_elements(By.CLASS_NAME,"stu-name")
         print(len(elements))
         for i in range(1,len(elements)+1):
             xname="// *[ @ id = 'studentInfo'] / tbody / tr["+str(i)+"] / td[2]"
             xid="// *[ @ id = 'studentInfo'] / tbody / tr["+str(i)+"] / td[1] / i"
-            if self.driver.find_element(By.XPATH,xname).text==studentinfo.Sname:
-                print(self.driver.find_element(By.XPATH,xid).text)
-                self.driver.find_element(By.XPATH,xid).click()
+            if driver.find_element(By.XPATH,xname).text==studentinfo.Sname:
+                print(driver.find_element(By.XPATH,xid).text)
+                driver.find_element(By.XPATH,xid).click()
 
-        self.driver.find_element(By.ID,"delete-group").click()
+        driver.find_element(By.ID,"delete-group").click()
         time.sleep(1)
-        self.driver.find_element(By.XPATH,"//*[@id='yx_messager_danger_3']/div[1]/div[3]/a[1]").click()
+        driver.find_element(By.XPATH,"//*[@id='yx_messager_danger_3']/div[1]/div[3]/a[1]").click()
         time.sleep(1)
-        self.driver.find_element(By.ID,"searchInfo").send_keys(studentinfo.Sname)
-        self.driver.find_element(By.ID,"searchInfo").send_keys(Keys.ENTER)
+        driver.find_element(By.ID,"searchInfo").send_keys(studentinfo.Sname)
+        driver.find_element(By.ID,"searchInfo").send_keys(Keys.ENTER)
         time.sleep(2)
         tag = True
-        elements2=self.driver.find_elements(By.CLASS_NAME,"stu-name")
+        elements2=driver.find_elements(By.CLASS_NAME,"stu-name")
         for element in elements2:
             if element.text==studentinfo.Sname:
                 tag=False
         self.assertTrue(tag,"删除学生未成功")
 
     def test5_checkteachermanage(self):
-        self.logi.login(self,self.driver,yjdata.loginname,yjdata.loginpasswd)
+        driver.get("http://ct.yunxiao.com:8110")
         time.sleep(3)
-        self.driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
+        driver.find_element(By.CLASS_NAME,"header__schoolBase").click()
         time.sleep(2)
-        self.driver.find_element(By.CLASS_NAME,"teacherInfo").click()
+        driver.find_element(By.CLASS_NAME,"teacherInfo").click()
         time.sleep(2)
-        content=self.driver.find_element(By.ID,"downloadTeacherTemplate").text
+        content=driver.find_element(By.ID,"downloadTeacherTemplate").text
         self.assertIn('老师',content,"教师页面显示不正常")
 
 
